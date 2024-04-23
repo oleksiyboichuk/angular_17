4db76fcceb803db7b4fd2439affe60c57fa91e99
Alex <alex@example.com>
2024-04-23 08:33:55 -0700

    Continue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroesComponent, CommonModule, MessagesComponent],
+  imports: [HeroesComponent, CommonModule, MessagesComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+	path: 'heroes',
+	loadComponent: () => import('./heroes/heroes.component')
+		.then(c => c.HeroesComponent)
+}];
